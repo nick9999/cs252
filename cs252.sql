6e95512380102df4f2fff625311c366e48fd16e4
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2014-11-15 09:21:03 +0530

    database name added to sqp

--- cs252.sql
+++ cs252.sql
@@ -16,6 +16,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+create database if not exists `cs252`;
+use cs252; 
+
 --
 -- Database: `cs252`
 --
