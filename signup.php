17ef3761d4d29aaadb3e600dca370d65f6f7e900
raunakshamnani <raunakshamnani@users.noreply.github.com>
2014-11-26 04:21:47 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php
+
 $name = $_POST['name'];
 $username = $_POST['username'];
 $email = $_POST['email'];
@@ -6,23 +7,23 @@ $flag = $_POST['flag'];
 $password = $_POST['password'];
 $passpass = $_POST['repassword'];
 
-include_once 'connect-db.php';
+$mysqli = new mysqli("localhost","root","root","cs252");
 
-if(empty($name)||empty($username)||empty($email)||empty($password))
+if(empty($name) || empty($username) || empty($email) || empty($password) || empty($passpass))
 {
-	echo "<script>alert('Name, username, email or password field cannot be left empty!!');</script>";
-	header('Location: signup.php'); //have to change the name
+	echo "<script>alert(\'Name, username, email or password field cannot be left empty!!\');</script>";
+	header('Location: register.php');
 }
 else if(empty($flag))
 {
-	echo "Cannot determine whether teacher or student<br>";
+	echo "<script>alert('Cannot determine whether teacher or student');</script>";
 }
 else
 {
 	if($password!=$passpass)
 	{
 		echo "<script>alert('Password and confirm password do not match');</script>";
-		header('Location: signup.html');		
+		header('Location: register.php');
 	}
 	$temp = strip_tags(stripslashes($name));
 	$check_name = ($temp==$name);
@@ -31,64 +32,39 @@ else
 	$temp = strip_tags(stripslashes($password));
 	$check_pass = ($temp==$password);
 	$check_email = filter_var($email,FILTER_VALIDATE_EMAIL);
+
 	if(!$check_name||!$check_username||!$check_pass||$check_email!=$email)
 	{
 		echo "<script>alert('Name, username, email or password field cannot contain \,< or >!!');</script>";
 	}
 	else
 	{
-		echo "<script>alert(\"Username etc feild are full\")</script>";
-		$captcha=1;
 		$sql = $mysqli->prepare("SELECT * from users where email = ? or username = ?");
 		$sql->bind_param('ss', $email, $username);
 		$sql->execute();
 		$sql->store_result();
-		$check_phone=1;
-		if($flag=='t')
-		{
-			$temp = ereg("^[0-9 \-]$",$_POST['phone']);
-			if($_POST['phone']!=$temp)
-				$check_phone=0;
-		}
 
-		if($captcha && $sql->num_rows==0 && $check_phone)
+		if($sql->num_rows==0)
 		{
-			echo "<script>alert(\"Insert Query Initiated\")</script>";			
 			$sql = $mysqli->prepare("INSERT INTO users (name,username,email,password,tors,phone) values (?,?,?,PASSWORD(?),?,?)");
 			
 			$sql->bind_param('ssssss', $name, $username, $email, $password, $flag, $_POST['phone']);
 			if($sql->execute())
-			{	
+			{
 				echo "<script>alert(\"Successfully Registered\")</script>";
-				$worked = 1;
-				$doc = new DOMDocument();
-				$doc->loadHTML("<button onclick=\"window.location='login.php'\">Login</button>");
-				echo $doc->saveHTML();
+				header('Location: register.php');
 			}
 			else
 			{
 				echo "<script>alert(\"Failure. Try registering again\")</script>";
-				$worked=0;
 			}
 		}
-		else if(!$check_phone)
-			echo "Please enter correct phone number<br>";
 		else
-			echo "Username or email already registered.<br>";
-	}
-}
+		{
+			echo "<script>alert(\"Username Already Exist\")</script>";
+			header('Location: register.php');
+		}
 
-function checkCaptcha()
-{
-	session_start();
-	$captcha=1;
-	if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"]=='')  
-	{
-		echo  '<strong>Incorrect captcha code.</strong>';
-		$c=0;
 	}
-	else
-		$c=1;
-	return $c; 
 }
 ?>
